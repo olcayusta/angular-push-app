346503a393e6594bb46d7ed7853f5fbd390874af
olcay <olcayusta02@gmail.com>
2020-08-13 16:38:20 +0300

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ServiceWorkerModule, SwPush, SwUpdate} from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSnackBar, MatSnackBarModule} from '@angular/material';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {PushNotificationService} from './push-notification.service';
+import {delay, retryWhen, tap} from 'rxjs/operators';
 
 @NgModule({
   declarations: [
@@ -10,9 +17,45 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSnackBarModule,
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(update: SwUpdate, push: SwPush, snackbar: MatSnackBar, pushService: PushNotificationService) {
+    update.available.subscribe((val) => {
+      console.log('update available');
+      const snack = snackbar.open('Update Available', 'Reload');
+
+      snack.onAction().subscribe(() => {
+        window.location.reload();
+      });
+    });
+
+    push.messages.subscribe(msg => {
+      console.log(msg);
+      snackbar.open(JSON.stringify(msg));
+    });
+
+    const key = 'BO2o-qUJJEbSYGL7BcPlHAtEU0cPdW5OGrAJcm-swR4GYZl6_OK7lFGrQaHXtQtlHMG6V5f72hU_ug6kg4A_voo';
+    push.requestSubscription({serverPublicKey: key})
+      .then(pushSubscription => {
+        console.log(pushSubscription.toJSON());
+        pushService.sendSubscriptionToTheServer(pushSubscription).pipe(
+          retryWhen(errors =>
+            errors.pipe(
+              tap(err => {
+                console.error('Got error', err);
+              }),
+              delay(1000)
+            )
+          )
+        ).subscribe();
+      });
+  }
+}
---
+++ src/app/push-notification.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PushNotificationService {
+
+  constructor(private http: HttpClient) { }
+
+  public sendSubscriptionToTheServer(subscription: PushSubscription) {
+    return this.http.post('//localhost:5000/subscribe', subscription);
+  }
+}
